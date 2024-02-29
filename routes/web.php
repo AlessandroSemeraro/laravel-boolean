e79ee387714374df29ba6f5bbdff8070f8a3425e
AlessandroSemeraro <alesemeraro1994@gmail.com>
2024-02-29 18:31:18 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\CocktailController as GuestCocktailController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [GuestCocktailController::class, 'index']);
+
+
+Route::name('cocktail.')->group(
+    function () {
+        route::get('/listCocktails', [GuestCocktailController::class, 'index'])->name('index');
+        route::get('/listCocktails/{cocktail}', [GuestCocktailController::class, 'show'])->name('show');
+    }
+);
